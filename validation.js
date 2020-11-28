e83e7372c7e9e106b0ca958f485a4ab315256aaa
toto1384 <totoalex62@gmail.com>
2020-11-28 21:05:12 +0200

    v1.17

--- validation.js
+++ validation.js
@@ -9,12 +9,12 @@ const activityValidation= (data)=>{
         icon: Joi.number().required(),
         tags: Joi.array().items(Joi.string()),
         color: Joi.string().required(),
-        trackedStart: Joi.array().items(Joi.date().format('DD MM YYYY HH:mm:ss').raw()),
-        trackedEnd: Joi.array().items(Joi.date().format('DD MM YYYY HH:mm:ss').raw()),
+        trackedStart: Joi.array().items(Joi.date().timestamp('javascript')),
+        trackedEnd: Joi.array().items(Joi.date().timestamp('javascript')),
         parentId:Joi.number(),
         description:Joi.string().allow(''),
         value:Joi.number(),
-        blacklistedDates:Joi.array().items(Joi.date().format('DD MM YYYY HH:mm:ss').raw()),
+        blacklistedDates:Joi.array().items(Joi.date().timestamp('javascript')),
         valueMultiply:Joi.number(),
         schedules:Joi.array().items(Joi.string())
     })
@@ -44,16 +44,16 @@ const taskValidation= (data)=>{
     const taskSchema = Joi.object({
         _id:Joi.string(),
         name: Joi.string().required().allow(''),
-        checks: Joi.array().items(Joi.date().format('DD MM YYYY HH:mm:ss').raw()),
+        checks: Joi.array().items(Joi.date().timestamp('javascript')),
         tags: Joi.array().items(Joi.string()),
         color: Joi.string().required(),
-        trackedStart: Joi.array().items(Joi.date().format('DD MM YYYY HH:mm:ss').raw()),
-        trackedEnd: Joi.array().items(Joi.date().format('DD MM YYYY HH:mm:ss').raw()),
+        trackedStart: Joi.array().items(Joi.date().timestamp('javascript')),
+        trackedEnd: Joi.array().items(Joi.date().timestamp('javascript')),
         parentId:Joi.number(),
         isParentCalendar: Joi.boolean(),
         description:Joi.string().allow(''),
         value:Joi.number(),
-        blacklistedDates:Joi.array().items(Joi.date().format('DD MM YYYY HH:mm:ss').raw()),
+        blacklistedDates:Joi.array().items(Joi.date().timestamp('javascript')),
         valueMultiply:Joi.number(),
         schedules:Joi.array().items(Joi.string())
     })
@@ -67,11 +67,11 @@ const taskValidation= (data)=>{
 const scheduledValidation= (data)=>{
     const scheduledSchema = Joi.object({
         _id:Joi.string(),
-        startTime: Joi.date().format('DD MM YYYY HH:mm:ss').raw(),
+        startTime: Joi.date().timestamp('javascript'),
         duration: Joi.number().min(0),
         repeatRule: Joi.number(),
         repeatValue: Joi.string(),
-        repeatUntil: Joi.date().format('DD MM YYYY HH:mm:ss').raw(),
+        repeatUntil: Joi.date().timestamp('javascript'),
     })
 
     return scheduledSchema.validate(data);
